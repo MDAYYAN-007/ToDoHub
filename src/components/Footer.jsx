2b0aa37251638247a2c3565a0b654e1680023a9c
M DAYYAN <dayyanmohammed6@gmail.com>
2024-09-18 21:46:49 +0530

    Remove Contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,21 +9,21 @@ const Footer = () => {
           <h3>About Us</h3>
           <p className="abt">We help you manage your tasks efficiently and effectively. Stay organized with our web app!</p>
         </div>
-        <div className="footer-section">
+        {/* <div className="footer-section">
           <h3>Contact</h3>
           <p>Email: <a href="mailto:mdayyanofficial@gmail.com">mdayyanofficial@gmail.com</a></p>
           <p>Phone: <a href="tel:+917483621525">+91 7483621525</a></p>
-        </div>
+        </div> */}
         <div className="footer-section">
           <h3>Follow Us</h3>
           <div className="social-links">
             <a href="https://www.instagram.com/mddayyan007/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-instagram"></i>
             </a>
-            <a href="https://www.linkedin.com/in/mohammed-dayyan-6718852aa" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/mohammed-dayyan-6718852aa/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-linkedin"></i>
             </a>
-            <a href="https://github.com/mdAYYAN-007" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/mdAYYAN-007/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-github"></i>
             </a>
           </div>
